dc81e5d2cb48db202f53c0556c3e80f69dd6fb14
dell <bencharniaamal2@gmail.com>
2021-03-29 18:01:14 +0100

    amal is committing

---
+++ src/app/playlistes/playlistes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-playlistes',
+  templateUrl: './playlistes.component.html',
+  styleUrls: ['./playlistes.component.css']
+})
+export class PlaylistesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
